f03d95924caa830db8b9bd02cf3f9909ca347c16
Rico Sonntag <mail@ricosonntag.de>
2025-10-23 11:32:10 +0200

    refactor: add content identifier key constant (#79)

--- src/Model/QuickTimeMeta.php
+++ src/Model/QuickTimeMeta.php
@@ -17,6 +17,11 @@ namespace MagicSunday\ImageMeta\Model;
 final readonly class QuickTimeMeta
 {
     /**
+     * QuickTime metadata key used for the content identifier value.
+     */
+    public const CONTENT_IDENTIFIER_KEY = 'com.apple.quicktime.content.identifier';
+
+    /**
      * Creates a new instance of QuickTime metadata information.
      *
      * @param array<string, string|int|float|bool> $keys Map of QuickTime metadata keys and their values.
@@ -32,7 +37,7 @@ final readonly class QuickTimeMeta
      */
     public function contentIdentifier(): ?string
     {
-        $key = 'com.apple.quicktime.content.identifier';
+        $key = self::CONTENT_IDENTIFIER_KEY;
 
         return isset($this->keys[$key]) ? (string) $this->keys[$key] : null;
     }
