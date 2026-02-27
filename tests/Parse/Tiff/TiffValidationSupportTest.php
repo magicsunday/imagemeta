0d2173137d1a24bd8676e22ce6dbac7bb0181df8
Rico Sonntag <mail@ricosonntag.de>
2026-02-27 21:54:02 +0100

    GH-2013: Add Red Tests For BitsPerSample Consolidation

--- tests/Parse/Tiff/TiffValidationSupportTest.php
+++ tests/Parse/Tiff/TiffValidationSupportTest.php
@@ -11,8 +11,14 @@ declare(strict_types=1);
 
 namespace MagicSunday\ImageMeta\Tests\Parse\Tiff;
 
+use MagicSunday\ImageMeta\Core\MemoryBuffer;
+use MagicSunday\ImageMeta\Core\ParseError;
+use MagicSunday\ImageMeta\Exif\Model\ExifNumericList;
 use MagicSunday\ImageMeta\Exif\Model\ExifTag;
+use MagicSunday\ImageMeta\Exif\Model\Ifd;
+use MagicSunday\ImageMeta\Exif\Model\IfdEntry;
 use MagicSunday\ImageMeta\Model\Tiff\TiffTag;
+use MagicSunday\ImageMeta\Parse\Tiff\TiffConst;
 use MagicSunday\ImageMeta\Parse\Tiff\TiffValidationSupport;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
@@ -34,5 +40,53 @@ final class TiffValidationSupportTest extends TestCase
     public function countedImageDataTagNameFallsBackToHexTagLabel(): void
     {
         self::assertSame('IFD tag 0x1234', TiffValidationSupport::countedImageDataTagName(0x1234));
+    }
+
+    #[Test]
+    public function resolveUniformBitsPerSampleReturnsUniformValue(): void
+    {
+        $support = new TiffValidationSupport(new MemoryBuffer("\0"));
+        $ifd     = new Ifd([
+            ExifTag::BITS_PER_SAMPLE => new IfdEntry(
+                ExifTag::BITS_PER_SAMPLE,
+                TiffConst::TYPE_SHORT,
+                3,
+                new ExifNumericList([12, 12, 12]),
+            ),
+        ]);
+
+        self::assertSame(12, $support->resolveUniformBitsPerSample($ifd, 'UnitTest', 3000));
+    }
+
+    #[Test]
+    public function resolveUniformBitsPerSampleRejectsNonIntegerComponents(): void
+    {
+        $this->expectException(ParseError::class);
+        $this->expectExceptionCode(3001);
+
+        $support = new TiffValidationSupport(new MemoryBuffer("\0"));
+        $ifd     = new Ifd([
+            ExifTag::BITS_PER_SAMPLE => new IfdEntry(
+                ExifTag::BITS_PER_SAMPLE,
+                TiffConst::TYPE_SHORT,
+                2,
+                new ExifNumericList([8, 8.5]),
+            ),
+        ]);
+
+        $support->resolveUniformBitsPerSample($ifd, 'UnitTest', 3000);
+    }
+
+    #[Test]
+    public function requiresSingleBitsPerSampleIntegerComponentErrorTemplateInSource(): void
+    {
+        $source = \file_get_contents(__DIR__ . '/../../../src/Parse/Tiff/TiffValidationSupport.php');
+
+        self::assertNotFalse($source);
+        self::assertSame(
+            1,
+            \substr_count($source, 'BitsPerSample must decode to integer components for %s.'),
+            'Expected duplicated BitsPerSample integer-component error template to be consolidated.',
+        );
     }
 }
