2bf536f395aef15817cb272ae41786c57716ff7a
Rico Sonntag <mail@ricosonntag.de>
2026-02-27 20:00:25 +0100

    GH-1828: Consolidate Grouped Imports In StreamWindow Test

--- tests/Core/StreamWindowTest.php
+++ tests/Core/StreamWindowTest.php
@@ -11,15 +11,21 @@ declare(strict_types=1);
 
 namespace MagicSunday\ImageMeta\Tests\Core;
 
-use MagicSunday\ImageMeta\Core\BoundsError;
-use MagicSunday\ImageMeta\Core\ByteReader;
-use MagicSunday\ImageMeta\Core\Stream;
-use MagicSunday\ImageMeta\Core\StreamWindow;
-use MagicSunday\ImageMeta\Core\Util\UInt64;
-use MagicSunday\ImageMeta\Core\Util\Unpack;
-use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\Test;
-use PHPUnit\Framework\Attributes\UsesClass;
+use MagicSunday\ImageMeta\Core\{
+    BoundsError,
+    ByteReader,
+    Stream,
+    StreamWindow,
+};
+use MagicSunday\ImageMeta\Core\Util\{
+    UInt64,
+    Unpack,
+};
+use PHPUnit\Framework\Attributes\{
+    CoversClass,
+    Test,
+    UsesClass,
+};
 use PHPUnit\Framework\TestCase;
 
 use function pack;
